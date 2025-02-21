e022b9d8c79170b66d5ad0c2b58c8002575eb4ee
mozilianulb <mozilla.mobile.bm@gmail.com>
2025-02-21 17:58:25 +0200

    Create trigger_todo.py

---
+++ trigger_todo.py
@@ -0,0 +1,13 @@
+# TODO: Handle error if response is bad (fixme)
+def process_data():
+    pass  # Placeholder function that needs implementation
+
+# TODO: Optimize this loop (fixme)
+for i in range(10):
+    print(f"Processing item {i}")
+
+class DataHandler:
+    # TODO: Add proper exception handling (fixme)
+    def handle_data(self, data):
+        if data is None:
+            pass  # This should raise an exception
